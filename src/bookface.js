0e5325d7bd2d5c4e03a4cdbeb5235782be676f9a
Maksym Butsykin <butsmax@gmail.com>
2015-02-21 20:39:48 +0200

    refactor

--- src/bookface.js
+++ src/bookface.js
@@ -1,18 +1,17 @@
 'use strict';
 
 (function() {
-    var handlebars,
+    var i18n,
 
-        i18n,
         bookFace,
         server,
         router,
         properties;
 
-    handlebars = require('hbs');
-    i18n = require('config/i18n');
+    i18n = require('i18n');
 
     require('config/passport');
+    require('config/modules');
 
     bookFace = (function() {
         server = require('server');
@@ -21,13 +20,6 @@
 
         server.use(router);
         server.use(i18n.init);
-
-        handlebars.registerHelper('__', function () {
-            return i18n.__.apply(this, arguments);
-        });
-        handlebars.registerHelper('__n', function () {
-            return i18n.__n.apply(this, arguments);
-        });
 
         return {
             httpServer: server,
