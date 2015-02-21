167bd511c7cceb13fcda42b7613159bea77f6815
margarita1989 <ritanik.6.6.6@gmail.com>
2015-02-22 00:22:49 +0200

    main_menu

--- www/ROOT/components/upload/upload-controller.js
+++ www/ROOT/components/upload/upload-controller.js
@@ -3,17 +3,5 @@ define([''], function() {
 
           console.log($http);
 
-//        var app = angular.module('app', [
-//            'lr.upload'
-//        ]);
-//        console.log('hello');
-//
-//        angular.element(document).ready(function () {
-//            angular.bootstrap(document, ['app']);
-//
-//            console.log('hello');
-//            console.log(User);
-//
-//        });
     }];
 });
