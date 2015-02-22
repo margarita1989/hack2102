bc142385338ce75362ea3debd494fee4a7c52242
Maksym Butsykin <butsmax@gmail.com>
2015-02-22 04:43:26 +0200

    Book Rating

--- src/controllers/ProfileController.js
+++ src/controllers/ProfileController.js
@@ -2,23 +2,27 @@
 
 (function() {
     var ProfileCtrl,
+        UserDTO,
         User;
 
     User = require('models/User');
+    UserDTO = require('utils/UserDTO');
 
     ProfileCtrl = function(req, res, next) {
+        var newUserDTO;
+
         function getUser(userID, user) {
             User.findOne({'customID': userID}, function(err, user_profile) {
                 res.render('profile', {
-                    isLoggedIn: (user_profile) ? true : false,
-                    user: user || {},
+                    isLoggedIn: (user) ? true : false,
+                    user: user || null,
                     user_profile: user_profile
                 });
             });
         }
 
         if(req.cookies['token']) {
-            User.findOne({'google.token': req.cookies['token']}, function(err, user) {
+            newUserDTO = new UserDTO({'google.token': req.cookies['token']}, function(err, user) {
                 getUser(req.params['userID'], user);
             });
         } else {
